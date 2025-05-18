a810dfa6f86f980ca7fcda595d6574576921df42
RONLUG <ronnakorn.lugsamipichate@gmail.com>
2025-05-18 15:12:06 +0700

    Up to step 4: before add house pricing model

--- app/app.py
+++ app/app.py
@@ -1,23 +1,43 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort, redirect
 import pickle
 import numpy as np
+import sys
+import json
 
 app = Flask(__name__)
+
 
 # Load the trained model
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
+
+@app.errorhandler(400)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 400
+
 @app.route("/")
 def home():
-    return "ML Model is Running"
+    return redirect("https://youtu.be/dQw4w9WgXcQ?si=2_n6XLTHgPFGnptv", code=302)
+
+@app.route("/health", methods=["POST"])
+def health():
+    return jsonify({"status": "ok"})
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    input_features = np.array(data["features"]).reshape(1, -1)
-    prediction = model.predict(input_features)
-    return jsonify({"prediction": int(prediction[0])})
+    if "features" not in data:
+        abort(400, description="Can't access data.features from data")
+    try: 
+        if np.array(data["features"]).shape[1] != 4:
+            abort(400, description="Invalid data.features format, data.features must be array with shape [n, 4]")
+    except:
+        abort(400, description="Invalid data.features format, data.features must be array with shape [n, 4]")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9000) #check your port number ( if it is in use, change the port number)
+    input_features = np.array(data["features"]).reshape(2, -1)
+    prediction = model.predict_proba(input_features)
+    pred_results = zip(np.argmax(prediction, 1).tolist(), np.max(prediction, 1).tolist())
+    json_key = ["prediction", "confidence"]
+    return jsonify([dict(zip(json_key, pred)) for pred in pred_results])
